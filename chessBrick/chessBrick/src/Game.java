3d25528bb3c9f511b6d4697fa515f98e21393cfd
PerpetualForce <chris.superstar.spencer1600@gmail.com>
2024-02-20 16:27:28 -0500

    <UPDATE notation debugging>
    not done yet

--- chessBrick/chessBrick/src/Game.java
+++ chessBrick/chessBrick/src/Game.java
@@ -31,16 +31,17 @@ public class Game {
 			printBoard(b);
 			b.evalBoard();
 			if (isPlayerTurn) {
-				System.out.println("Please type inputs as curX,curY;tgtX,tgtY, eg. 1,2;3,4");
+				//System.out.println("Please type inputs as curX,curY;tgtX,tgtY, eg. 1,2;3,4");
 				String s = reader.readLine();
-				int curX, curY, tgtX, tgtY;
+				notation(s, b);
+				/*int curX, curY, tgtX, tgtY;
 				curX = s.charAt(0) - 48;
 				curY = s.charAt(2) - 48;
 				tgtX = s.charAt(4) - 48;
 				tgtY = s.charAt(6) - 48;
 				if (b.board[curX][curY].makeMovePlayer(tgtX, tgtY)) {
 					isPlayerTurn = false;
-				}
+				}*/
 			} else {
 				for (Piece p : b.onBoard) {
 					for (DeltaMovement d : p.legalMoves()) {
@@ -54,28 +55,13 @@ public class Game {
 		}
 	}
 
-	public static int[][] notation(String notation, Board b) {
+	public static boolean notation(String notation, Board b) {
 		// Step 1: convert human notation into coordinates
-		int[][] coordinates = new int[2][2];
 		if (notation.length() < 2) {
-			coordinates[0][0] = -1;
-			coordinates[0][1] = -1;
-			coordinates[1][0] = -1;
-			coordinates[1][1] = -1;
-			return coordinates;
+			return false;
 		}
 		
-		for (int i = 0; i < notation.length(); i++) {
-			if (notation.charAt(i) == ' ') {
-				notation = notation.substring(0,i) + notation.substring(i+1, notation.length());
-				i--;
-			}
-		}
-
-
-		int[] ranks = {0,1,2,3,4,5,6,7};
-
-		char[] files = {'a','b','c','d','e','f','g','h'};
+		notation = notation.replaceAll(" ", "");
 		Map<Character, Integer> filesToNum = new HashMap<>();
 
 		filesToNum.put('a', 0);
@@ -88,13 +74,14 @@ public class Game {
 		filesToNum.put('h', 7);
 
 
-		if (isLower(notation.charAt(0))) {
+		if (Character.isLowerCase(notation.charAt(0))) {
 			int xcoord = filesToNum.get(notation.charAt(0));
-			int ycoord = 7 - (int) notation.charAt(1);
+			int ycoord = 7 - Character.getNumericValue(notation.charAt(1)) + 1;
+			
 			
 			if (notation.charAt(notation.length()-2) == '=') {
 				for (int i= -1; i <= 1; i++) {
-					if ((xcoord + i < 8 && xcoord + i >= 0) && b.board[xcoord+i][6].makeMovePlayer(xcoord, ycoord)) {
+					if ((xcoord + i < 8 && xcoord + i >= 0) && unNull(b.board[xcoord+i][6]) && b.board[xcoord+i][6].makeMovePlayer(xcoord, ycoord)) {
 						char newPiece = notation.charAt(notation.length()-1);
 						if (newPiece == 'N') {
 							b.board[xcoord+i][7] = new Knight(xcoord+i, 7, 'n', b);
@@ -110,12 +97,12 @@ public class Game {
 				}
 			}
 			else if (notation.charAt(1) == 'x') {
-				if ((xcoord > 0 && b.board[xcoord-1][ycoord-1].makeMovePlayer(xcoord, ycoord)) || (xcoord < 7 && b.board[xcoord-1][ycoord+1].makeMovePlayer(xcoord, ycoord))) {
+				if ((unNull(b.board[xcoord-1][ycoord-1]) && xcoord > 0 && b.board[xcoord-1][ycoord-1].makeMovePlayer(xcoord, ycoord)) || (unNull(b.board[xcoord-1][ycoord+1]) && xcoord < 7 && b.board[xcoord-1][ycoord+1].makeMovePlayer(xcoord, ycoord))) {
 					isPlayerTurn = false;
 				}
 			}  else {
 				for (int i = -2; i < 0; i++) {
-					if (b.board[xcoord+i][ycoord].makeMovePlayer(xcoord, ycoord)) {
+					if (unNull(b.board[xcoord][ycoord-i]) && b.board[xcoord][ycoord-i].makeMovePlayer(xcoord, ycoord)) {
 						isPlayerTurn = false;
 					}
 				}
@@ -132,18 +119,22 @@ public class Game {
 				notation = notation.substring(0, notation.length()-1);
 			}
 			int x = filesToNum.get(notation.charAt(notation.length()-2));
-			int y = 7 - (int) notation.charAt(notation.length()-1);
+			int y = 7-Character.getNumericValue(notation.charAt(notation.length() - 1))+1;
+
+
 			ArrayList<Integer> coordinate = generateMove(Character.toLowerCase(notation.charAt(0)), x, y, b);
 			if (coordinate.size() > 2) {
 				if (Character.isLetter(notation.charAt(1))) {
 					int xc = filesToNum.get(notation.charAt(1));
+
 					for (int k = 0; k < coordinate.size(); k+=2) {
 						if (coordinate.get(k) == xc) {
 							b.board[coordinate.get(k)][coordinate.get(k+1)].makeMovePlayer(x, y);
 						}
 					}
 				} else {
-					int yc = 7 - (int) notation.charAt(1);
+					int yc = 7-Character.getNumericValue(notation.charAt(1))+1;
+
 					for (int k = 0; k < coordinate.size(); k+=2) {
 						if (coordinate.get(k) == yc) {
 							b.board[coordinate.get(k-1)][coordinate.get(k)].makeMovePlayer(x, y);
@@ -154,28 +145,11 @@ public class Game {
 				b.board[coordinate.get(0)][coordinate.get(1)].makeMovePlayer(x, y);
 			}
 		} else {
-			coordinates[0][0] = -1;
-			coordinates[0][1] = -1;
-			coordinates[1][0] = -1;
-			coordinates[1][1] = -1;
-			return coordinates;
+			return false;
 		}
 
-
-		/*
-		 * Possibilities: Ne8, Nxe4, Nbxd4, N5xd4, Nbd7
-		 */
-
-		// Step 3: check if legal move
-
-
-
-		return coordinates;
+		return true;
 	}
-
-	/*public int quickEval(Board b) {
-		// player
-	}*/
 
 	public static boolean isLower(char c) {
         // Check if the character is lowercase by comparing it with its lowercase counterpart
@@ -192,20 +166,24 @@ public class Game {
 	public static ArrayList<Integer> generateMove(char tag, int x, int y, Board b) {
 		ArrayList<Integer> coordinates = new ArrayList<>();
 		for (int i = 0; i < b.onBoard.size(); i++) {
-			for (int j = 0; j < b.onBoard.get(i).moves.size(); i++) {
-				if (b.onBoard.get(i).tag == tag) {
-					ArrayList<DeltaMovement> possible = b.onBoard.get(i).legalMoves();
-					for (int k = 0; k < possible.size(); k++) {
-						if (possible.get(i).dx == x && possible.get(i).dy == y) {
-							coordinates.add(b.onBoard.get(i).xPos);
-							coordinates.add(b.onBoard.get(i).xPos);
-						}
+			if (b.onBoard.get(i).tag == tag) {
+				ArrayList<DeltaMovement> possible = b.onBoard.get(i).legalMoves();
+				for (int k = 0; k < possible.size(); k++) {
+					if (possible.get(k).dx == x && possible.get(k).dy == y) {
+						coordinates.add(b.onBoard.get(i).xPos);
+						coordinates.add(b.onBoard.get(i).xPos);
 					}
 				}
-				
 			}
 		}
 		return coordinates;
+	}
+
+	public static boolean unNull(Piece p) {
+		if (p == null) {
+			return false;
+		}
+		return true;
 	}
 
 
